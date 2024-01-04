1f991c38ab3ff0134c2d056c5a3f83af09b689ea
shmz <85086833+shmzzzz@users.noreply.github.com>
2024-01-04 11:27:55 +0900

    feat: Riverpodを使用して計算ができるようにする (#4)
    * ✨ Riverpodを使用して計算する

---
+++ lib/result.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'result.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$resultHash() => r'73f6d8b25e13d0df403f098c95c4efe589d9a72f';
+
+/// See also [Result].
+@ProviderFor(Result)
+final resultProvider = AutoDisposeNotifierProvider<Result, double>.internal(
+  Result.new,
+  name: r'resultProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$resultHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Result = AutoDisposeNotifier<double>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
