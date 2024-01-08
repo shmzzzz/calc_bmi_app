9486b3dace00fc477746b2f6e7cb07b4c75f5ff0
shmz <85086833+shmzzzz@users.noreply.github.com>
2024-01-08 11:42:23 +0900

    improve: 定数化する (#10)
    * 🚧 🎨 定数に切り出す
    * 🚧 🎨 定数を追加
    * 🎨 定数化する

---
+++ lib/constants/dimens.dart
@@ -0,0 +1,21 @@
+// marginとかpaddingとかfontSizeとか定義したい
+
+class Paddings {
+  static const double padding_16 = 16;
+  static const double padding_40 = 40;
+}
+
+class Margins {}
+
+class BoxSize {
+  static const double size_8 = 8;
+  static const double size_24 = 24;
+  static const double size_32 = 32;
+  static const double size_200 = 200;
+}
+
+class TextSize {
+  static const double size_16 = 16;
+  static const double size_20 = 20;
+  static const double size_48 = 48;
+}
